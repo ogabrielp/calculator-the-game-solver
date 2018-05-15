87dbd8ec89c96975d2a41b2d5e432560b00a242a
Gabriel Pinheiro <gabrielpcosta@gmail.com>
2018-05-15 09:18:32 -0300

    Add class 'Operation' - I realized that there was duplicated code in two differente files - code that was really similar but not precisely equal. When this happens, it generally means an abstraction can be done, and that's what I did. The logic for validating buttons was incredibly similar to the logic the 'Calculator' class used to perform a certain operation, so I decided to move this logic to another class, and use this class in both the validate_buttons function and in the Calculator class.

---
+++ Operation.py
@@ -0,0 +1,46 @@
+class Operation:
+    """
+    The class that centralizes operation info.
+    """
+
+    @staticmethod
+    def get_operation(operation):
+        """
+        Gets the function representing an operation.
+
+        :param operation: (string) the operation the user wants the function for.
+        :return: the lambda function for the operation.
+        """
+
+        def is_numeric(value):
+            """
+            Determines if a string is an integer number.
+
+            :param value: (string) the value to test.
+            :return: a boolean informing if it was possible to parse the string.
+            """
+            try:
+                value = int(value)
+                return True
+            except:
+                return False
+
+        # Basic operations
+        if operation.startswith(('+', '-', 'x', '/')):
+            # Strip the operator from the operation string
+            operator = operation[0]
+            operand = operation[1:]
+            if is_numeric(operand):
+                # If the operand is a numeric string, parse it as an integer
+                # and returns the function that represents the operation.
+                operand = int(operand)
+                operations = {
+                    '+': lambda value: value + operand,
+                    '-': lambda value: value - operand,
+                    'x': lambda value: value * operand,
+                    '/': lambda value: value / operand
+                }
+                return operations[operator]
+            return None
+
+        return None
